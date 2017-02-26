05c784807828ace0a8b30cc101a6bc792b9734bc
Lloyd Hanneman <lhanneman@Lloyds-MacBook-Pro.local>
2017-02-25 19:52:31 -0600

    LH updated service to call api directly

--- Website/baby-gallery-app/src/app/services/images.service.ts
+++ Website/baby-gallery-app/src/app/services/images.service.ts
@@ -17,23 +17,23 @@ export class ImagesService {
     }
 
 // need to update web api to return the cors header for allow-access-same-origin or wahtever
-    //getImages(): Observable<Array<ImageModel>> {
-    //    return this.http.get('http://landon-image-api.azurewebsites.net/images').map(resp => resp.json());
-    //}
-
-    getImages(): Observable<ImageModel[]> {
-        //[{"Url":"https://landonaugust.blob.core.windows.net/pictures/IMG_0138.JPG"}]
-        let imgs = new Array<ImageModel>();
-        let img = new ImageModel();
-        img.url = 'https://landonaugust.blob.core.windows.net/pictures/IMG_0138.JPG';
-        imgs.push(img);
-
-        let img2 = new ImageModel();
-        img2.url = 'https://landonaugust.blob.core.windows.net/pictures/IMG_0125.JPG';
-        imgs.push(img2);
-
-        return Observable.of(imgs);
+    getImages(): Observable<Array<ImageModel>> {
+       return this.http.get('http://landon-image-api.azurewebsites.net/images').map(resp => resp.json());
     }
+
+    //getImages(): Observable<ImageModel[]> {
+        
+        // let imgs = new Array<ImageModel>();
+        // let img = new ImageModel();
+        // img.url = 'https://landonaugust.blob.core.windows.net/pictures/IMG_0138.JPG';
+        // imgs.push(img);
+
+        // let img2 = new ImageModel();
+        // img2.url = 'https://landonaugust.blob.core.windows.net/pictures/IMG_0125.JPG';
+        // imgs.push(img2);
+
+        // return Observable.of(imgs);
+    //}
 
 
 
